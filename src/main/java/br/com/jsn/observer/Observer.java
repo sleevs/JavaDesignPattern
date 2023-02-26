6e4134b2e7a71dea4d21230acd5836dba9f03495
Jeison Muniz <jsnprogramer@gmail.com>
2023-02-26 11:53:11 +0000

    Delete src directory

--- src/main/java/br/com/jsn/observer/Observer.java
+++
@@ -1,7 +0,0 @@
-package br.com.jsn.observer;
-
-public abstract class Observer {
-
-    protected Subject subject;
-    public abstract void update();
-}
--- src/main/java/br/com/jsn/strategy/Strategy.java
+++
@@ -1,5 +0,0 @@
-package br.com.jsn.strategy;
-
-public interface Strategy {
-    void order();
-}
