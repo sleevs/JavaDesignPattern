2d2b69f9c7affddeeb6c5551c34fc924857acba8
Jeison Muniz <jsnprogramer@gmail.com>
2024-02-21 23:48:01 -0300

    imeplementation data component to domain action

--- jsn-composite/src/main/java/br/com/jsn/repository/ActionRepository.java
+++ jsn-composite/src/main/java/br/com/jsn/repository/ActionRepository.java
@@ -1,5 +1,19 @@
 package br.com.jsn.repository;
 
-public class ActionRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.jsn.entity.ActionEntity;
+
+public interface ActionRepository extends JpaRepository<ActionEntity, Long> {
     
+
+    @Query(value="SELECT * FROM action a WHERE a.action_id = ?" , nativeQuery=true)
+    public ActionEntity findActionById(Long id);
+
+    @Query(value="SELECT * FROM action a WHERE a.task_id = ?" , nativeQuery=true)
+    public List<ActionEntity> findActionsByTaskId(Long id);
+
 }
