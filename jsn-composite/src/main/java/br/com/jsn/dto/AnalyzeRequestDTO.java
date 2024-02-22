d1c31129e70408dafec6f8a2a06f24b952ddf7b0
Jeison Muniz <jsnprogramer@gmail.com>
2024-02-22 12:06:09 -0300

    implementation of data layer

---
+++ jsn-composite/src/main/java/br/com/jsn/dto/AnalyzeRequestDTO.java
@@ -0,0 +1,94 @@
+package br.com.jsn.dto;
+
+public class AnalyzeRequestDTO {
+
+    
+    private Long employeeId;
+    private String type;
+    private Long taskId;
+    private Long projectId;
+    private String value ;
+    private String cost;
+    private String time;
+    private String complexity;
+    private String priority;
+
+    public AnalyzeRequestDTO(){}
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public void setCost(String cost) {
+        this.cost = cost;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getComplexity() {
+        return complexity;
+    }
+
+    public void setComplexity(String complexity) {
+        this.complexity = complexity;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    
+
+
+    
+}
