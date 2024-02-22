c52ae825aef666629a964482773902d9dea15859
Jeison Muniz <jsnprogramer@gmail.com>
2024-02-21 23:53:06 -0300

    imeplementation data component to domain action

--- jsn-composite/src/main/java/br/com/jsn/service/ActionService.java
+++ jsn-composite/src/main/java/br/com/jsn/service/ActionService.java
@@ -1,7 +1,22 @@
 package br.com.jsn.service;
 import org.springframework.stereotype.Service;
 
+import br.com.jsn.dto.ActionDTO;
+import br.com.jsn.entity.ActionEntity;
+
 @Service
 public class ActionService {
+
+
+    public ActionDTO build(ActionEntity e){
+
+        return null;
+    }
+    
+
+    public ActionEntity build(ActionDTO dto){
+
+        return null;
+    }
     
 }
