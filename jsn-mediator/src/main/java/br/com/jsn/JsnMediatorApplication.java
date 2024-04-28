9c3d4d5172fd16e182b8e20f163db2b7735804b1
Jeison Muniz <jsnprogramer@gmail.com>
2024-04-28 14:40:21 -0300

    structure to the project

---
+++ jsn-mediator/src/main/java/br/com/jsn/JsnMediatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.jsn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsnMediatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsnMediatorApplication.class, args);
+	}
+
+}
