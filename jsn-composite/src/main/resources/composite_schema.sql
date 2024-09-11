48ae4f7b2ddfd5a39c6b63c1515be08e4ebf34c4
Jeison Muniz <jsnprogramer@gmail.com>
2024-09-11 00:27:38 -0300

    feat: implementation of doc

--- jsn-composite/src/main/resources/composite_schema.sql
+++ jsn-composite/src/main/resources/composite_schema.sql
@@ -1,52 +1,10 @@
 
-INSERT INTO public.account
-(account_id, account_name, accout_email, account_phone, account_document)
-VALUES(nextval('account_account_id_seq'::regclass), '', '', '', '');
-
-INSERT INTO public.account
-(account_id, account_name, accout_email, account_phone, account_document)
-VALUES(nextval('account_account_id_seq'::regclass), 'James', 'james@gmail.com', '21987654123', '12345678996');
-
-INSERT INTO public.project
-(project_id, project_type, project_description, project_date, project_scope, account_id)
-VALUES(nextval('project_project_id_seq'::regclass), 'Construção', 'Costruir uma casa de dois comodos',  'Construção', 1);
-
-
-SELECT * FROM project
-FULL JOIN analysis
-ON project.project_id = analysis.project_id
-FULL JOIN task 
-ON  analysis.analyze_id = task.analyze_id ;
-
-
-
-INSERT INTO public.employee
-(employee_id, employee_name, employee_type, employee_phone, employee_email, employee_experience, employee_skill, employee_certification, employee_create)
-VALUES(nextval('employee_employee_id_seq'::regclass), '', '', '', '', '', '', '', '');
-
-
-
-
-INSERT INTO public.analysis
-(analyze_id, analyze_value, analyze_cost, analyze_estimate, analyze_date, analyze_complexity, employee_id, project_id, analyze_status)
-VALUES(nextval('analysis_analyze_id_seq'::regclass), '', '', 0, '', '', 0, 0, '');
-
-
-
-
-
-INSERT INTO public.task
-(task_id, task_functional, task_description, task_requirements, analyze_id, task_type, task_status)
-VALUES(nextval('task_task_id_seq'::regclass), '', '', '', 0, '', '');
-
-
-
 
 
 create table account (
 	account_id serial primary key,
 	account_name varchar(50) not null ,
-	accout_email varchar(50) ,
+	account_email varchar(50) ,
 	account_phone varchar (20),
 	account_document varchar (50)
   
@@ -117,26 +75,5 @@ CREATE TABLE action (
   task_id INT  
  
 );
-
-
-
-
-CREATE TABLE resource (
-  resource_id SERIAL PRIMARY KEY, 
-  resource_description VARCHAR (50)  , 
-  resource_value VARCHAR (20)  , 
-  resource_type VARCHAR (20)   
- 
-);
-
-
-
-CREATE TABLE description_action (
-  description_action_id SERIAL PRIMARY KEY, 
-  description_action_value VARCHAR (20)  NOT NULL, 
-  description_action_type VARCHAR (20)  NOT NULL
- 
-);
-
 
 
